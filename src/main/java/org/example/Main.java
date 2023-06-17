7f852c91a47a574645ef74c78f00fc35ebcd9eb2
vika_t <selga1991@yandex.ru>
2023-06-17 23:36:26 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount;
+        boolean registered;
+        long bonus = service.calculate(5_000, true);
+        System.out.println(bonus);
+    }
+}
